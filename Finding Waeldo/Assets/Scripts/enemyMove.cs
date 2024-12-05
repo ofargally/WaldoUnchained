03ee5266cf6ebd18fc314e8aaff88df489a0623a
Jennifer <jenniferwu0525@gmail.com>
2024-12-05 17:40:03 +0100

    level design

---
+++ Finding Waeldo/Assets/Scripts/enemyMove.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class enemyMove : MonoBehaviour
+{
+    public float movementSpeed;
+
+    private float movedDistance;
+
+    public float range = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        movementSpeed /= 60f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (movedDistance > range || movedDistance < (0-range))
+        {
+            movementSpeed = 0 - movementSpeed;
+            movedDistance = 0;
+        }
+        transform.position += new Vector3(0f, 0f, movementSpeed);
+        movedDistance += movementSpeed;
+    }
+}
