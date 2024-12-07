691e4be42cc5fb6ce6195adb6a79b7ca7cbb4d64
ofargally <ofargally@colgate.edu>
2024-12-07 16:50:44 +0100

    weapon

--- Assets/Scripts/WeaponShoot.cs
+++ Assets/Scripts/WeaponShoot.cs
@@ -4,10 +4,42 @@ public class WeaponShoot : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     // Update is called once per frame
+    public Camera playerCamera;
+
+    //Shooting
+    public bool isShooting, readyToShoot;
+    public float shootingDelay = 2f;
+    bool allowReset = true;
+
+
+
+    //Burst
+    public int currentBurst;
+    public int bulletsPerBurst = 3;
+
+    //Spread
+    public float spreadIntensity;
+
+    //Bullet
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
     public float bulletSpeed = 30.0f;
     public float bulletLife = 3.0f;
+
+    // Shootingmode
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+
+    private void Awake()
+    {
+        currentMode = ShootingMode.Single;
+    }
+
+    public ShootingMode currentMode;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Mouse0))
